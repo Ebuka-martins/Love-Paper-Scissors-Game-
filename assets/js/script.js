10586ac027a5709a1744ba700405e96a6886a248
Ebuka MartinsEchesi <martin4best@gmail.com>
2024-07-27 11:53:31 +0000

    git: adding code for result using javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,9 +11,21 @@ function playGame(playerdecision) {
 
     if (playerdecision === machinedecision) {
         result = "IT'S A DRAW GAME!!!";
-    } else {
+    } 
+    else {
         switch(playerdecision) {
             case "rock":
+                result = (machinedecision === "scissors") ? "YOU WIN!" : "YOU LOSE!";
+                break; 
+            case "paper":
+                 result = (machinedecision === "rock") ? "YOU WIN!" : "YOU LOSE!";
+                 break;  
+             case "scissors":
+                 result = (machinedecision === "paper") ? "YOU WIN!" : "YOU LOSE!";
+                  break;         
         }
     }
+    playerDisplay.textContent = `Player: ${playerdecision}`;
+    machineDisplay.textContent = `Machine: ${machinedecision}`;
+    resultDisplay.textContent = result;
 }
