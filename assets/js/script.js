deaf1f146884f1b2e7558c78ded49b841c9d42cb
Ebuka MartinsEchesi <martin4best@gmail.com>
2024-07-27 14:44:15 +0000

    git: updating styling using css and text using html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,42 @@
-
-
 const decisions = ['rock', 'paper', 'scissors'];
 const playerDisplay = document.getElementById('playerDisplay');
 const machineDisplay = document.getElementById('machineDisplay');
 const resultDisplay = document.getElementById('resultDisplay');
+const playerScoreDisplay = document.getElementById('playerScoreDisplay');
+const machineScoreDisplay = document.getElementById('machineScoreDisplay');
+
+let playerScore = 0;
+let machineScore = 0;
 
 function playGame(playerdecision) {
-    const machinedecision = decisions[Math.floor (Math.random() * 3)];
+    const machinedecision = decisions[Math.floor(Math.random() * 3)];
     let result = "";
 
     if (playerdecision === machinedecision) {
         result = "IT'S A DRAW GAME!!!";
-    } 
-    else {
-        switch(playerdecision) {
+    } else {
+        switch (playerdecision) {
             case "rock":
                 result = (machinedecision === "scissors") ? "YOU WIN!" : "YOU LOSE!";
-                break; 
+                break;
             case "paper":
-                 result = (machinedecision === "rock") ? "YOU WIN!" : "YOU LOSE!";
-                 break;  
-             case "scissors":
-                 result = (machinedecision === "paper") ? "YOU WIN!" : "YOU LOSE!";
-                  break;         
+                result = (machinedecision === "rock") ? "YOU WIN!" : "YOU LOSE!";
+                break;
+            case "scissors":
+                result = (machinedecision === "paper") ? "YOU WIN!" : "YOU LOSE!";
+                break;
         }
     }
+
+    if (result === "YOU WIN!") {
+        playerScore++;
+    } else if (result === "YOU LOSE!") {
+        machineScore++;
+    }
+
     playerDisplay.textContent = `Player: ${playerdecision}`;
     machineDisplay.textContent = `Machine: ${machinedecision}`;
     resultDisplay.textContent = result;
+    playerScoreDisplay.textContent = playerScore;
+    machineScoreDisplay.textContent = machineScore;
 }
