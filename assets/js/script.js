e2eba4d5b6f5ed38b558109a04d63731b9265e30
Ebuka MartinsEchesi <martin4best@gmail.com>
2024-07-27 10:37:35 +0000

    git: seeting up a javascript for the game

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,19 @@
 
+
+const decisions = ['rock', 'paper', 'scissors'];
+const playerDisplay = document.getElementById('playerDisplay');
+const machineDisplay = document.getElementById('machineDisplay');
+const resultDisplay = document.getElementById('resultDisplay');
+
+function playGame(playerdecision) {
+    const machinedecision = decisions[Math.floor (Math.random() * 3)];
+    let result = "";
+
+    if (playerdecision === machinedecision) {
+        result = "IT'S A DRAW GAME!!!";
+    } else {
+        switch(playerdecision) {
+            case "rock":
+        }
+    }
+}
